684f7f8a7902bf33476433cd29562baf88c22bf8
oAnthonyG <anthonytguilherme@hotmail.com>
2023-11-08 15:13:23 -0300

    add a readme with days

---
+++ 7_dia_desafio.rb
@@ -0,0 +1,14 @@
+# 7° dia - Função
+
+### Dificuldade: ⭐☆☆☆☆
+
+### **Descrição do Problema**
+
+# Crie uma função que recebe dois parametros, nome e idade, imprima esses valores no formato: "*nome, idade*" - **obs: deve ser usada uma função para realizar o exercício**
+
+
+def pessoa(nome, idade)
+  puts "#{nome}, #{idade}"
+end
+
+pessoa('Breach', 45)
